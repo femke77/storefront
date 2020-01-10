3d79246d44dc8fbdefe4c9ab93777fe5346e33b0
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-09 23:35:17 -0800

    customer prompts, stock check, update stock and price check working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,25 +7,74 @@ var connection = mysql.createConnection({
     user: "root",
     password: "2hearts",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Welcome to Bamazon! Here are our available goods for today today.")
     runBamazon();
-  });
+});
 
-  function runBamazon(){
-    connection.query("SELECT * FROM products", function(err, res){
+function runBamazon() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         res.forEach(e => {
             console.log(`Item ID: ${e.item_id}
 Product name: ${e.product_name}
-Department: ${e.department_name}
 Price: $${e.price.toFixed(2)}
-*******************`);
+*******************`)
         });
+        inquire();
     });
+}
 
-  }
+function inquire() {
+    inquirer.prompt([
+        {
+            message: "Enter the item number of the product you wish to purchase.",
+            name: "itemID",
+            validate: function (name) {
+                if (isNaN(name) || name < 1 || name > 10) {
+                    throw "Enter a number between 1 and 10 please.";
+                }
+                return true;
+            }
+        },
+        {
+            message: "Enter the amount you wish to buy.",
+            name: "quantity",
+            validate: function(name){
+                if (isNaN(name)){
+                    throw "Enter a number please."
+                }
+                return true;
+            }
+        }
+    ]).then(function(ans) {
+        processOrder(ans);
+    })
+}
+
+function processOrder(ans){
+    //get quantity of the chosen item from db
+    //check if user quanity > actual stock
+    //if not enough stock, alert user
+    //if enough stock, update the db quanity and give total price
+
+    var query = "SELECT stock_quantity, price FROM products WHERE ?"
+    connection.query(query, {item_id: ans.itemID}, function(err, res){
+        if (err) throw err;
+        if (ans.quantity > res[0].stock_quantity){
+            console.log("Insufficient quantity available to process order.")
+        } else {
+            var remaining = res[0].stock_quantity - parseInt(ans.quantity);
+            var total = res[0].price * parseInt(ans.quantity);
+            query = "UPDATE products SET ? WHERE ?"
+            connection.query(query, [{stock_quantity: remaining},{item_id: ans.itemID}], function(err, res){
+                if (err) throw err;
+                console.log(`Your order is processed and your total is $${total.toFixed(2)}`)
+            })
+        }
+    })
+}
 
